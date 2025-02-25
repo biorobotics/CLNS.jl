8076bdd971cda4fe6494e75c3ec5925f829f2a24
Anoop Bhat <noopy.g.bhat@gmail.com>
2025-02-24 19:45:17 -0500

    Fixed issue resulting in nans in cost computation

--- src/utilities.jl
+++ src/utilities.jl
@@ -58,21 +58,23 @@ end
 function splice!(tour::Tour, i::Int64, cost::Float64)
   prev_i = prev_tour(tour, i)
   deleted_point = tour[i]
-  tour.cost = tour.cost - tour.cost_seq[prev_i] - tour.cost_seq[i] + cost
+  # tour.cost = tour.cost - tour.cost_seq[prev_i] - tour.cost_seq[i] + cost # Results in NaN when we have infinite costs
   tour.tour = cat(tour.tour[1:i-1, :], tour.tour[i + 1:end, :], dims=1)
   splice!(tour.set_seq, i)
   tour.cost_seq[prev_i] = cost
   splice!(tour.cost_seq, i)
+  tour.cost = sum(tour.cost_seq)
   return deleted_point
 end
 
 function insert!(tour::Tour, i::Int64, point::Vector{Float64}, set::Int64, pre_cost::Float64, post_cost::Float64)
   prev_i = prev_tour(tour, i)
-  tour.cost = tour.cost - tour.cost_seq[prev_i] + pre_cost + post_cost
+  # tour.cost = tour.cost - tour.cost_seq[prev_i] + pre_cost + post_cost # Results in NaN when we have infinite costs
   tour.tour = cat(tour.tour[1:i-1, :], point', tour.tour[i:end, :], dims=1) # perform the insertion
   insert!(tour.set_seq, i, set)
   tour.cost_seq[prev_i] = pre_cost
   insert!(tour.cost_seq, i, post_cost)
+  tour.cost = sum(tour.cost_seq)
 end
 
 # function length(m::Matrix)
